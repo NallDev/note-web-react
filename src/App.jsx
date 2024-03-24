de7621a15d63b8906ab4a5d12e5f09a09f3daddc
afrinaldiGenzo <afrinaldi190401@gmail.com>
2024-03-24 23:02:09 +0700

    ADD : react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,104 +2,115 @@ import React from "react"
 import { NoteForm } from "./components/note_form"
 import { SearchBar } from "./components/search_bar"
 import { NoteList } from "./components/note_llist"
-import { getInitialData } from "./utils"
+import Navigation from "./components/Navigation"
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes: getInitialData(),
-            title: "",
-            description: "",
-            query: "",
-        }
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             notes: getInitialData(),
+//             title: "",
+//             description: "",
+//             query: "",
+//         }
 
-        this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this)
-        this.onDescriptionChangeEventHandler = this.onDescriptionChangeEventHandler.bind(this)
-        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this)
-        this.onDeleteHandler = this.onDeleteHandler.bind(this)
-        this.onQueryChangeEventHandler = this.onQueryChangeEventHandler.bind(this)
-        this.onArchiveHandler = this.onArchiveHandler.bind(this)
-    }
+//         this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this)
+//         this.onDescriptionChangeEventHandler = this.onDescriptionChangeEventHandler.bind(this)
+//         this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this)
+//         this.onDeleteHandler = this.onDeleteHandler.bind(this)
+//         this.onQueryChangeEventHandler = this.onQueryChangeEventHandler.bind(this)
+//         this.onArchiveHandler = this.onArchiveHandler.bind(this)
+//     }
 
-    onTitleChangeEventHandler(event) {
-        const newTitle = event.target.value
-        if (newTitle.length <= 50) {
-            this.setState({ title: newTitle })
-        }
-    }
+//     onTitleChangeEventHandler(event) {
+//         const newTitle = event.target.value
+//         if (newTitle.length <= 50) {
+//             this.setState({ title: newTitle })
+//         }
+//     }
 
-    onDescriptionChangeEventHandler(event) {
-        this.setState({ description: event.target.value })
-    }
+//     onDescriptionChangeEventHandler(event) {
+//         this.setState({ description: event.target.value })
+//     }
 
-    onSubmitEventHandler(event) {
-        event.preventDefault()
-        if (this.state.title && this.state.description) {
-            const newNote = {
-                id: +new Date(),
-                title: this.state.title,
-                body: this.state.description,
-                createdAt: new Date().toISOString(),
-                archived: false,
-            }
-            this.setState((prevState) => ({
-                notes: [...prevState.notes, newNote],
-                title: "",
-                description: "",
-            }))
-        } else {
-            alert("Title and description are required.")
-        }
-    }
+//     onSubmitEventHandler(event) {
+//         event.preventDefault()
+//         if (this.state.title && this.state.description) {
+//             const newNote = {
+//                 id: +new Date(),
+//                 title: this.state.title,
+//                 body: this.state.description,
+//                 createdAt: new Date().toISOString(),
+//                 archived: false,
+//             }
+//             this.setState((prevState) => ({
+//                 notes: [...prevState.notes, newNote],
+//                 title: "",
+//                 description: "",
+//             }))
+//         } else {
+//             alert("Title and description are required.")
+//         }
+//     }
 
-    onDeleteHandler(id) {
-        const notes = this.state.notes.filter((note) => note.id !== id)
-        this.setState({ notes })
-    }
+//     onDeleteHandler(id) {
+//         const notes = this.state.notes.filter((note) => note.id !== id)
+//         this.setState({ notes })
+//     }
 
-    onQueryChangeEventHandler(event) {
-        this.setState({ query: event.target.value })
-    }
+//     onQueryChangeEventHandler(event) {
+//         this.setState({ query: event.target.value })
+//     }
 
-    onArchiveHandler(id) {
-        this.setState((prevState) => ({
-            notes: prevState.notes.map((note) => (note.id === id ? { ...note, archived: !note.archived } : note)),
-        }))
-    }
+//     onArchiveHandler(id) {
+//         this.setState((prevState) => ({
+//             notes: prevState.notes.map((note) => (note.id === id ? { ...note, archived: !note.archived } : note)),
+//         }))
+//     }
 
-    render() {
-        return (
-            <div className="flex flex-col items-center justify-center min-h-screen p-4">
-                <div className="w-full text-center">
-                    <h1 className="text-3xl font-bold mb-4">NOTE WEB</h1>
-                    <NoteForm
-                        onTitleChange={this.onTitleChangeEventHandler}
-                        onDescriptionChange={this.onDescriptionChangeEventHandler}
-                        onSubmit={this.onSubmitEventHandler}
-                        title={this.state.title}
-                        description={this.state.description}
-                    />
-                    <div className="border-t border-gray-400 my-8 w-full"></div>
-                    <SearchBar onQueryChange={this.onQueryChangeEventHandler} query={this.state.query} />
-                </div>
-                <h2 className="text-3xl font-bold my-4">Your Note</h2>
-                <NoteList
-                    notes={this.state.notes.filter(
-                        (note) => note.title.toLowerCase().includes(this.state.query.toLowerCase()) && note.archived === false,
-                    )}
-                    onDelete={this.onDeleteHandler}
-                    onArchive={this.onArchiveHandler}
-                />
-                <h2 className="text-3xl font-bold my-4">Your Archive</h2>
-                <NoteList
-                    notes={this.state.notes.filter((note) => note.title.toLowerCase().includes(this.state.query.toLowerCase()) && note.archived)}
-                    onDelete={this.onDeleteHandler}
-                    onArchive={this.onArchiveHandler}
-                />
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div className="flex flex-col items-center justify-center min-h-screen p-4">
+//                 <div className="w-full text-center">
+//                     <h1 className="text-3xl font-bold mb-4">NOTE WEB</h1>
+//                     <NoteForm
+//                         onTitleChange={this.onTitleChangeEventHandler}
+//                         onDescriptionChange={this.onDescriptionChangeEventHandler}
+//                         onSubmit={this.onSubmitEventHandler}
+//                         title={this.state.title}
+//                         description={this.state.description}
+//                     />
+//                     <div className="border-t border-gray-400 my-8 w-full"></div>
+//                     <SearchBar onQueryChange={this.onQueryChangeEventHandler} query={this.state.query} />
+//                 </div>
+//                 <h2 className="text-3xl font-bold my-4">Your Note</h2>
+//                 <NoteList
+//                     notes={this.state.notes.filter(
+//                         (note) => note.title.toLowerCase().includes(this.state.query.toLowerCase()) && note.archived === false,
+//                     )}
+//                     onDelete={this.onDeleteHandler}
+//                     onArchive={this.onArchiveHandler}
+//                 />
+//                 <h2 className="text-3xl font-bold my-4">Your Archive</h2>
+//                 <NoteList
+//                     notes={this.state.notes.filter((note) => note.title.toLowerCase().includes(this.state.query.toLowerCase()) && note.archived)}
+//                     onDelete={this.onDeleteHandler}
+//                     onArchive={this.onArchiveHandler}
+//                 />
+//             </div>
+//         )
+//     }
+// }
+
+function App() {
+    return (
+        <>
+            <header>
+                <h1>Note App</h1>
+                <Navigation />
+            </header>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        <img src="/images/home.svg" alt="home" />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/archive">
+                        <img src="/images/archive.svg" alt="archive" />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/add">
+                        <img src="/images/add.svg" alt="add" />
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
